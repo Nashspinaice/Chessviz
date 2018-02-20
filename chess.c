99fc362c45d7f81f9962c4baed43450162542646
Nashspinaice <nashspinaice60699@mail.ru>
2018-02-20 10:45:33 +0700

    chess code

---
+++ chess.c
@@ -0,0 +1,7 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+main()
+{
+	printf("8 L H E K Q E H L\n7 S S S S S S S S \n6 \n5 \n4 \n3 \n2 S S S S S S S S \n1 L H E K Q E H L \n  a b c d e f g h");
+}
