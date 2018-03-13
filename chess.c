f7ccc516a2b634dc67026173dded0f3c68435d4c
Makson <makson@localhost.localdomain>
2018-03-13 14:22:49 +0700

    Black and white chess

--- chess.c
+++ chess.c
@@ -3,5 +3,5 @@
 
 main()
 {
-	printf("8 L H E K Q E H L\n7 S S S S S S S S \n6 \n5 \n4 \n3 \n2 S S S S S S S S \n1 L H E K Q E H L \n  a b c d e f g h");
+	printf("8 L H E K Q E H L\n7 S S S S S S S S \n6 \n5 \n4 \n3 \n2 s s s s s s s s \n1 l h e k q e h l \n  a b c d e f g h");
 }
